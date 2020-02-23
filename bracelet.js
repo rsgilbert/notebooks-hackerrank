37e498e2ed46e98fd686ea3aec7a5bc679abc353
Gilbert <gssenyonjo@gmail.com>
2020-02-23 22:37:42 +0300

    Solved the bracelet challenge

--- bracelet.js
+++ bracelet.js
@@ -1,6 +1,6 @@
 const median = arr => {
   const mid = Math.floor(arr.length / 2),
-    nums = arr
+    nums = arr.s
   return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
 };
 
